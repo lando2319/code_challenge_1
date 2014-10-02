856032003ab46e89231dd53a7611c4ad8b9e1b30
Mike Land <mikepland@gmail.com>
2014-10-02 14:06:47 -0500

    Initial Commit

---
+++ code_challenge_1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  code_challenge_1
+//
+//  Created by MIKE LAND on 10/2/14.
+//  Copyright (c) 2014 MIKE LAND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ code_challenge_1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  code_challenge_1
+//
+//  Created by MIKE LAND on 10/2/14.
+//  Copyright (c) 2014 MIKE LAND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
